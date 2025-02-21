35855323cdb1d4410c5cc22c5d2737dc7f730eec
Quân Lê <quanle@Quans-MacBook-Pro-3.local>
2025-02-21 11:46:15 +0700

    Init Modules

---
+++ resources/views/index.blade.php
@@ -0,0 +1,8 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Blog') }}
+        </h2>
+    </x-slot>
+    {{ config('blog.name') }}
+</x-app-layout>
